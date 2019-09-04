1da879a11803d4a1ce4cf694d770e1be650284f7
honganan <honganan@youzan.com>
2019-09-04 17:54:25 +0800

    dubbo and youzan boot

--- benchmark-base/src/main/java/org/apache/dubbo/benchmark/serialize/SerializationOptimizerImpl.java
+++ benchmark-base/src/main/java/org/apache/dubbo/benchmark/serialize/SerializationOptimizerImpl.java
@@ -1,16 +1,16 @@
-package org.apache.dubbo.benchmark.serialize;
-
-import org.apache.dubbo.benchmark.bean.Page;
-import org.apache.dubbo.benchmark.bean.User;
-import org.apache.dubbo.benchmark.service.UserService;
-import org.apache.dubbo.common.serialize.support.SerializationOptimizer;
-
-import java.util.Arrays;
-import java.util.Collection;
-
-public class SerializationOptimizerImpl implements SerializationOptimizer {
-    @Override
-    public Collection<Class> getSerializableClasses() {
-        return Arrays.asList(User.class, Page.class, UserService.class);
-    }
-}
+//package org.apache.dubbo.benchmark.serialize;
+//
+//import org.apache.dubbo.benchmark.bean.Page;
+//import org.apache.dubbo.benchmark.bean.User;
+//import org.apache.dubbo.benchmark.service.UserService;
+//import org.apache.dubbo.common.serialize.support.SerializationOptimizer;
+//
+//import java.util.Arrays;
+//import java.util.Collection;
+//
+//public class SerializationOptimizerImpl implements SerializationOptimizer {
+//    @Override
+//    public Collection<Class> getSerializableClasses() {
+//        return Arrays.asList(User.class, aPage.class, UserService.class);
+//    }
+//}
