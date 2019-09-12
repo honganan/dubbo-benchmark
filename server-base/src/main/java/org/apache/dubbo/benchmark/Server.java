b98b7fc3f2927886f67f3813c9a9a4529f002725
honganan <honganan@youzan.com>
2019-09-12 14:45:27 +0800

    30 iteration

--- server-base/src/main/java/org/apache/dubbo/benchmark/Server.java
+++ server-base/src/main/java/org/apache/dubbo/benchmark/Server.java
@@ -1,10 +1,13 @@
 package org.apache.dubbo.benchmark;
 
+import com.youzan.aladdin.AladdinContainer;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Server {
 
     public static void main(String[] args) throws InterruptedException {
+        AladdinContainer.run(args);
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("provider.xml")) {
             context.start();
             Thread.sleep(Integer.MAX_VALUE);
